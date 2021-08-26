f3f274c30c307bba15076e8852cc6d17340809b0
DanarVirkan <danarvirkan@gmail.com>
2021-08-26 17:15:15 +0700

    updated test files for Comment Repository

--- src/Domains/comments/_test/CommentRepository.test.js
+++ src/Domains/comments/_test/CommentRepository.test.js
@@ -3,7 +3,7 @@ const CommentRepository = require('../CommentRepository');
 describe('CommentRepository interface', () => {
   it('should throw error when invoke abstract behavior', async () => {
     const commentRepository = new CommentRepository();
-    await expect(commentRepository.addCommentByThreadId('')).rejects.toThrowError('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(commentRepository.addCommentByThreadId({}, '', '')).rejects.toThrowError('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
     await expect(commentRepository.getCommentByThreadId('')).rejects.toThrowError('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
     await expect(commentRepository.getCommentOwner('')).rejects.toThrowError('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
     await expect(commentRepository.deleteCommentById('')).rejects.toThrowError('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
