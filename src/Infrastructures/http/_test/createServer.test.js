317eb808020d299f88b3ef4fb5d3bd98550c7c16
DanarVirkan <danarvirkan@gmail.com>
2021-08-26 11:46:58 +0700

    updated createServer test files

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -1,6 +1,15 @@
+const Jwt = require('@hapi/jwt');
+const ServerTestHelper = require('../../../../tests/ServerTestHelper');
+const ThreadTableTestHelper = require('../../../../tests/ThreadsTableTestHelper');
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const injections = require('../../injections');
 const createServer = require('../createServer');
 
 describe('HTTP server', () => {
+  afterEach(async () => {
+    await ThreadTableTestHelper.cleanTable();
+    await UsersTableTestHelper.cleanTable();
+  });
   it('should response 404 when request unregistered route', async () => {
     // Arrange
     const server = await createServer({});
@@ -36,5 +45,43 @@ describe('HTTP server', () => {
     expect(response.statusCode).toEqual(500);
     expect(responseJson.status).toEqual('error');
     expect(responseJson.message).toEqual('terjadi kegagalan pada server kami');
+  });
+  it('should handle jwt properly return error when no credentials', async () => {
+    const requestPayload = {
+      title: 'judul thread',
+      body: 'isi thread',
+    };
+    const server = await createServer({});
+
+    const response = await server.inject({
+      method: 'POST',
+      url: '/threads',
+      payload: requestPayload,
+    });
+
+    expect(response.statusCode).toEqual(401);
+  });
+  it('should handle jwt properly', async () => {
+    const requestPayload = {
+      title: 'judul thread',
+      body: 'isi thread',
+    };
+
+    const server = await createServer(injections);
+    const accessToken = await ServerTestHelper.getAccessToken();
+    const response = await server.inject({
+      method: 'POST',
+      url: '/threads',
+      payload: requestPayload,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    expect(accessToken).toBeDefined();
+    const responseJson = JSON.parse(response.payload);
+    expect(response.statusCode).toEqual(201);
+    expect(responseJson.data.addedThread).toBeDefined();
+    expect(responseJson.data.addedThread.title).toEqual(requestPayload.title);
   });
 });
