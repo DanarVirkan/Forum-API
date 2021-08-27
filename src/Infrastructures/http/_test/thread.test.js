ab71f5d8c8bde2c1fc1403bfb50812dc7275a4ee
DanarVirkan <danarvirkan@gmail.com>
2021-08-27 11:42:58 +0700

    updated new routes, handler, and test from threads

--- src/Infrastructures/http/_test/thread.test.js
+++ src/Infrastructures/http/_test/thread.test.js
@@ -5,6 +5,7 @@ const pool = require('../../database/postgres/pool');
 const injections = require('../../injections');
 const createServer = require('../createServer');
 const CommentsTableTestHelper = require('../../../../tests/CommentsTableTestHelper');
+const RepliesTableTestHelper = require('../../../../tests/RepliesTableTestHelper');
 
 describe('/threads endpoint', () => {
   afterAll(async () => {
@@ -12,6 +13,7 @@ describe('/threads endpoint', () => {
   });
 
   afterEach(async () => {
+    await RepliesTableTestHelper.cleanTable();
     await CommentTableTestHelper.cleanTable();
     await ThreadTableTestHelper.cleanTable();
     await UsersTableTestHelper.cleanTable();
@@ -111,6 +113,9 @@ describe('/threads endpoint', () => {
       await UsersTableTestHelper.addUser({ id: userId });
       await ThreadTableTestHelper.addThread(threadId, {}, userId);
       await CommentsTableTestHelper.addComment('comment-123', {}, userId, threadId);
+      await CommentsTableTestHelper.addComment('comment-666', {}, userId, threadId, true);
+      await RepliesTableTestHelper.addReply('reply-123', {}, userId, 'comment-123');
+      await RepliesTableTestHelper.addReply('reply-666', {}, userId, 'comment-123', true);
 
       const server = await createServer(injections);
       const response = await server.inject({
@@ -122,8 +127,238 @@ describe('/threads endpoint', () => {
       expect(response.statusCode).toEqual(200);
       expect(responseJson.status).toEqual('success');
       expect(responseJson.data.thread).toBeDefined();
+
       expect(responseJson.data.thread.comments).toBeDefined();
-      expect(responseJson.data.thread.comments).toHaveLength(1);
+      expect(responseJson.data.thread.comments).toHaveLength(2);
+      expect(responseJson.data.thread.comments[0].id).toBeDefined();
+      expect(responseJson.data.thread.comments[0].content).toBeDefined();
+      expect(responseJson.data.thread.comments[0].username).toBeDefined();
+      expect(responseJson.data.thread.comments[0].date).toBeDefined();
+
+      expect(responseJson.data.thread.comments[0].replies).toBeDefined();
+      expect(responseJson.data.thread.comments[0].replies).toHaveLength(2);
+      expect(responseJson.data.thread.comments[0].replies[0].content).toBeDefined();
+      expect(responseJson.data.thread.comments[0].replies[1].content).toEqual('**balasan telah dihapus**');
+    });
+  });
+  describe('when POST /threads/{threadId}/comments/{commentId}/replies', () => {
+    it('should response 201 when thread or comment exist', async () => {
+      const userId = 'user-123';
+      const threadId = 'thread-123';
+      const commentId = 'comment-123';
+      const payload = {
+        content: 'isi balasan',
+      };
+
+      await UsersTableTestHelper.addUser({ id: userId });
+      await ThreadTableTestHelper.addThread(threadId, {}, userId);
+      await CommentsTableTestHelper.addComment(commentId, {}, userId, threadId);
+
+      const server = await createServer(injections);
+      const response = await server.inject({
+        method: 'POST',
+        url: `/threads/${threadId}/comments/${commentId}/replies`,
+        payload,
+        auth: {
+          strategy: 'forum_jwt',
+          credentials: {
+            id: userId,
+          },
+        },
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(201);
+      expect(responseJson.status).toEqual('success');
+      expect(responseJson.data.addedReply).toBeDefined();
+    });
+    it('should response 404 when thread not exist', async () => {
+      const userId = 'user-123';
+      const threadId = 'thread-123';
+      const commentId = 'comment-123';
+      const payload = {
+        content: 'isi balasan',
+      };
+
+      await UsersTableTestHelper.addUser({ id: userId });
+
+      const server = await createServer(injections);
+      const response = await server.inject({
+        method: 'POST',
+        url: `/threads/${threadId}/comments/${commentId}/replies`,
+        payload,
+        auth: {
+          strategy: 'forum_jwt',
+          credentials: {
+            id: userId,
+          },
+        },
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(404);
+      expect(responseJson.status).toEqual('fail');
+    });
+    it('should response 404 when comment not exist', async () => {
+      const userId = 'user-123';
+      const threadId = 'thread-123';
+      const commentId = 'comment-123';
+      const payload = {
+        content: 'isi balasan',
+      };
+
+      await UsersTableTestHelper.addUser({ id: userId });
+      await ThreadTableTestHelper.addThread(threadId, {}, userId);
+
+      const server = await createServer(injections);
+      const response = await server.inject({
+        method: 'POST',
+        url: `/threads/${threadId}/comments/${commentId}/replies`,
+        payload,
+        auth: {
+          strategy: 'forum_jwt',
+          credentials: {
+            id: userId,
+          },
+        },
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(404);
+      expect(responseJson.status).toEqual('fail');
+    });
+    it('should response 401 when unauthorized', async () => {
+      const userId = 'user-123';
+      const threadId = 'thread-123';
+      const commentId = 'comment-123';
+      const payload = {
+        content: 'isi balasan',
+      };
+
+      await UsersTableTestHelper.addUser({ id: userId });
+      await ThreadTableTestHelper.addThread(threadId, {}, userId);
+      await CommentsTableTestHelper.addComment(commentId, {}, userId, threadId);
+
+      const server = await createServer(injections);
+      const response = await server.inject({
+        method: 'POST',
+        url: `/threads/${threadId}/comments/${commentId}/replies`,
+        payload,
+      });
+
+      expect(response.statusCode).toEqual(401);
+    });
+  });
+  describe('when DELETE /threads/{threadId}/comments/{commentId}/replies/{replyId}', () => {
+    it('should response 200 when reply exist', async () => {
+      const userId = 'user-123';
+      const threadId = 'thread-123';
+      const commentId = 'comment-123';
+      const replyId = 'reply-123';
+      const payload = {
+        content: 'isi balasan',
+      };
+
+      await UsersTableTestHelper.addUser({ id: userId });
+      await ThreadTableTestHelper.addThread(threadId, {}, userId);
+      await CommentsTableTestHelper.addComment(commentId, {}, userId, threadId);
+      await RepliesTableTestHelper.addReply(replyId, payload, userId, commentId);
+
+      const server = await createServer(injections);
+      const response = await server.inject({
+        method: 'DELETE',
+        url: `/threads/${threadId}/comments/${commentId}/replies/${replyId}`,
+        auth: {
+          strategy: 'forum_jwt',
+          credentials: {
+            id: userId,
+          },
+        },
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(200);
+      expect(responseJson.status).toEqual('success');
+    });
+    it('should response 404 when reply not exist', async () => {
+      const userId = 'user-123';
+      const threadId = 'thread-123';
+      const commentId = 'comment-123';
+      const replyId = 'reply-123';
+      const payload = {
+        content: 'isi balasan',
+      };
+
+      await UsersTableTestHelper.addUser({ id: userId });
+      await ThreadTableTestHelper.addThread(threadId, {}, userId);
+      await CommentsTableTestHelper.addComment(commentId, {}, userId, threadId);
+
+      const server = await createServer(injections);
+      const response = await server.inject({
+        method: 'DELETE',
+        url: `/threads/${threadId}/comments/${commentId}/replies/${replyId}`,
+        auth: {
+          strategy: 'forum_jwt',
+          credentials: {
+            id: userId,
+          },
+        },
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(404);
+      expect(responseJson.status).toEqual('fail');
+    });
+    it('should response 401 when unauthorized', async () => {
+      const userId = 'user-123';
+      const threadId = 'thread-123';
+      const commentId = 'comment-123';
+      const replyId = 'reply-123';
+      const payload = {
+        content: 'isi balasan',
+      };
+
+      await UsersTableTestHelper.addUser({ id: userId });
+      await ThreadTableTestHelper.addThread(threadId, {}, userId);
+      await CommentsTableTestHelper.addComment(commentId, {}, userId, threadId);
+      await RepliesTableTestHelper.addReply(replyId, payload, userId, commentId);
+
+      const server = await createServer(injections);
+      const response = await server.inject({
+        method: 'DELETE',
+        url: `/threads/${threadId}/comments/${commentId}/replies/${replyId}`,
+      });
+
+      expect(response.statusCode).toEqual(401);
+    });
+    it('should response 403 when when reply owner is not the user', async () => {
+      const userId = 'user-123';
+      const threadId = 'thread-123';
+      const commentId = 'comment-123';
+      const replyId = 'reply-123';
+      const fakeId = 'user-666';
+      const payload = {
+        content: 'isi balasan',
+      };
+
+      await UsersTableTestHelper.addUser({ id: userId });
+      await ThreadTableTestHelper.addThread(threadId, {}, userId);
+      await CommentsTableTestHelper.addComment(commentId, {}, userId, threadId);
+      await RepliesTableTestHelper.addReply(replyId, payload, userId, commentId);
+
+      const server = await createServer(injections);
+      const response = await server.inject({
+        method: 'DELETE',
+        url: `/threads/${threadId}/comments/${commentId}/replies/${replyId}`,
+        auth: {
+          strategy: 'forum_jwt',
+          credentials: {
+            id: fakeId,
+          },
+        },
+      });
+
+      expect(response.statusCode).toEqual(403);
     });
   });
 });
--- src/Interfaces/http/api/threads/handler.js
+++ src/Interfaces/http/api/threads/handler.js
@@ -4,8 +4,11 @@ class ThreadsHandler {
 
     this.postThreadHandler = this.postThreadHandler.bind(this);
     this.postCommentByThreadIdHandler = this.postCommentByThreadIdHandler.bind(this);
+    this.postReplyByThreadAndCommentIdHandler = this
+      .postReplyByThreadAndCommentIdHandler.bind(this);
     this.getThreadByIdHandler = this.getThreadByIdHandler.bind(this);
     this.deleteCommentByIdHandler = this.deleteCommentByIdHandler.bind(this);
+    this.deleteReplyByIdHandler = this.deleteReplyByIdHandler.bind(this);
   }
 
   async postThreadHandler(request, h) {
@@ -43,21 +46,48 @@ class ThreadsHandler {
     return response;
   }
 
+  async postReplyByThreadAndCommentIdHandler(request, h) {
+    const { id: userId } = request.auth.credentials;
+    const { threadId, commentId } = request.params;
+    await this._threadUseCase.getThreadById(threadId);
+    const { id, content, owner } = await this._threadUseCase
+      .addReplyByCommentId(request.payload, userId, commentId);
+    const response = h.response({
+      status: 'success',
+      data: {
+        addedReply: {
+          id,
+          content,
+          owner,
+        },
+      },
+    }).code(201);
+    return response;
+  }
+
   async getThreadByIdHandler(request, h) {
     const { threadId } = request.params;
     const {
-      id, title, body, date, owner,
+      id, title, body, date, username,
     } = await this._threadUseCase.getThreadById(threadId);
 
-    const username = await this._threadUseCase.getUsernameById(owner);
     const comments = await this._threadUseCase.getCommentByThreadId(threadId);
 
     const mappedComments = await Promise.all(comments.map(async ({
-      id: commentId, content, date: commentDate, owner: commentOwner, is_deleted: deleted,
+      id: commentId, content, date: commentDate, username: commentUsername, is_deleted: deleted,
     }) => {
-      const commentUsername = await this._threadUseCase.getUsernameById(commentOwner);
+      const replies = await this._threadUseCase.getReplyByCommentId(commentId);
+      const mappedReplies = replies.map(({
+        id: replyId,
+        content: replyContent,
+        date: replyDate,
+        username: replyUsername,
+        is_deleted: replyDeleted,
+      }) => ({
+        id: replyId, content: replyDeleted ? '**balasan telah dihapus**' : replyContent, date: replyDate, username: replyUsername,
+      }));
       return {
-        id: commentId, username: commentUsername, date: commentDate, content: deleted ? '**komentar telah dihapus**' : content,
+        id: commentId, username: commentUsername, date: commentDate, replies: mappedReplies, content: deleted ? '**komentar telah dihapus**' : content,
       };
     }));
 
@@ -78,9 +108,20 @@ class ThreadsHandler {
 
   async deleteCommentByIdHandler(request) {
     const { id: userId } = request.auth.credentials;
-    const { commentId } = request.params;
+    const { commentId, threadId } = request.params;
     await this._threadUseCase.verifyComment(commentId, userId);
-    await this._threadUseCase.deleteCommentById(commentId);
+    await this._threadUseCase.deleteComment(commentId, threadId);
+    return {
+      status: 'success',
+    };
+  }
+
+  async deleteReplyByIdHandler(request) {
+    const { id } = request.auth.credentials;
+    const { replyId, commentId, threadId } = request.params;
+
+    await this._threadUseCase.verifyReply(replyId, id);
+    await this._threadUseCase.deleteReply(replyId, commentId, threadId);
     return {
       status: 'success',
     };
